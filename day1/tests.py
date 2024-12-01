a1fa28f1fb61585ca989f41c5fdf298296a12d1a
Xiaoming <mail@zyming.me>
2024-12-01 18:59:06 +0100

    Change test-structure

--- day1/tests.py
+++ day1/tests.py
@@ -1,15 +1,14 @@
 import unittest
 
-from .solution import calc_distance
+from solution import calc_distance
 class Day1Tests(unittest.TestCase):
+    input = ("3  4\n"
+             "4  3\n"
+             "2  5\n"
+             "1  3\n"
+             "3  9\n"
+             "3  3")
     def test_problem1(self):
 
-        input = ("3  4\n"
-                 "4  3\n"
-                 "2  5\n"
-                 "1  3\n"
-                 "3  9\n"
-                 "3  3")
-
-        result = calc_distance.exec(input)
+        result = calc_distance.exec(self.input)
         self.assertEqual(result, 11)
