c982c3c5aabef66a6eb7dc5d566a2457ca18ea59
Ming <mail@zyming.me>
2024-12-02 13:37:52 +0100

    Add solution that passes test

--- day2/src/solutions.rs
+++ day2/src/solutions.rs
@@ -1,11 +1,72 @@
-type Report = Vec<u32>;
+type Report = Vec<i32>;
 
 enum ReportSafety {
     Unsafe,
     Safe(Report),
 }
 
+impl ReportSafety {
+    fn is_safe(&self) -> bool {
+        use ReportSafety::*;
+        if let Safe(_) = *self {
+            true
+        } else {
+            false
+        }
+    }
+}
+
 pub(crate) fn find_safe_reports(input: &str) -> u32 {
-    println!("this is the input\n{input}");
-    42
+    fn create_report(report_line: &str) -> Report {
+        report_line
+            .split_whitespace()
+            .map(|number| {
+                number
+                    .parse::<i32>()
+                    .unwrap_or_else(|input| panic!("Cannot parse: {:?}", input))
+            })
+            .collect()
+    }
+
+    fn check_safety(report: Report) -> ReportSafety {
+        fn test_is_ascending(report: &Report) -> (bool, &i32) {
+            if let Some(first) = report.get(0) {
+                if let Some(second) = report.get(1) {
+                    (first < second, second)
+                } else {
+                    panic!("invalid input");
+                }
+            } else {
+                panic!("invalid input");
+            }
+        }
+
+        let (is_ascending, mut elem) = test_is_ascending(&report);
+
+        fn unacceptable_diff(first: i32, second: i32) -> bool {
+            let diff = (first - second).abs();
+            diff < 1 || diff > 3
+        }
+        for num in &report[2..] {
+            if (unacceptable_diff(*num, *elem) || is_ascending && num < elem)
+                || (!is_ascending && num > elem)
+            {
+                println!("report: {:?} is Unsafe", report);
+                return ReportSafety::Unsafe;
+            }
+
+            elem = num
+        }
+
+        println!("report: {:?} is Safe", report);
+
+        return ReportSafety::Safe(report);
+    }
+
+    input
+        .lines()
+        .map(create_report)
+        .map(check_safety)
+        .filter(|report| report.is_safe())
+        .count() as u32
 }
