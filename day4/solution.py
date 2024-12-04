c65bb73ade3434a11d76acac82b8634ea4434778
Ming <mail@zyming.me>
2024-12-04 10:52:40 +0100

    Add solution that passes example for problem 2

--- day4/solution.py
+++ day4/solution.py
@@ -18,20 +18,41 @@ def count_xmas(input: str) -> int:
             if input_as_list[i][j].upper() == 'X':
                 for indices in direction_indices:
                     word_found = get_word_in_direction(input_as_list, indices, (i,j))
-                    if word_found.upper() == "XMAS":
+                    if word_found.upper() == "MAS":
                         xmas_count += 1
 
     return xmas_count
 
 def count_cross_mas(input: str) -> int:
-    return 0
+    input_as_list = [list(line) for line in input.splitlines()]
+
+    search_indices = [[(-1,-1),(1,1)], # Diagonal down left
+                      [(-1,1),(1,-1)], # Diagonal down right
+                      [(1,1),(-1,-1)], # Diagonal up right
+                      [(1,-1),(-1,1)], # Diagonal up left
+                     ]
+    x_mas_count = 0
+
+    for i in range(len(input_as_list)):
+        for j in range(len(input_as_list[i])):
+            if input_as_list[i][j].upper() == 'A':
+                potential_x = False
+                for indices in search_indices:
+                    word_found = get_word_in_direction(input_as_list, indices, (i,j))
+                    if word_found.upper() == "MS":
+                        if potential_x:
+                            x_mas_count += 1
+                        else:
+                            potential_x = True
+
+    return x_mas_count
 
 def get_word_in_direction(
         input: list[list[str]],
         indices: list[tuple[ int,int ]],
         startingpos: tuple[int, int]) -> str:
 
-    word = "X"
+    word = ""
     (i,j) = startingpos
     input_height = len(input)
     input_length = len(input[i])
