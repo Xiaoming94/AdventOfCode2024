9c84f02766f9929aa5cf42f585180de864066249
Xiaoming <mail@zyming.me>
2024-12-07 00:04:40 +0100

    Add complete solution for day6 problem 1

--- day6/src/day6main.rb
+++ day6/src/day6main.rb
@@ -1,3 +1,12 @@
 require_relative 'solution'
 
-puts 'hello'
+input = ''
+$stdin.each do |line|
+  input += line
+end
+
+puts "input is: \n#{input}"
+
+results = Solution.count_guard_steps(input)
+
+puts "problem1 solution: #{results}"
