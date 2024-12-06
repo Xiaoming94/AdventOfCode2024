38a36ccb431573c94f5e9998fb8e1053a2ad05fd
Xiaoming <mail@zyming.me>
2024-12-06 09:47:32 +0100

    Rubocop linter magic

--- day6/test/day6test.rb
+++ day6/test/day6test.rb
@@ -1,9 +1,11 @@
+# frozen_string
+
 require 'minitest/autorun'
-require_relative '../src/solution.rb'
+require_relative '../src/solution'
 
 class Day6Tests < Minitest::Test
   def test_framework
-    my_value = Solution::my_const_value()
+    my_value = Solution.my_const_value
     assert_equal 42, my_value
   end
 end
