b25fa80c57729a1579a8c8d6678c02449af760bc
Xiaoming <mail@zyming.me>
2024-12-07 21:23:40 +0100

    Solved problem 2 for example input

--- day7/src/solution.rs
+++ day7/src/solution.rs
@@ -24,6 +24,19 @@ fn create_equation(line: &str) -> Equation {
     }
 }
 
+fn concat_nums(lhs: RhsType, mut rhs: RhsType) -> RhsType {
+    let mut rhs_vec = Vec::new();
+
+    while rhs != 0 {
+        rhs_vec.push(rhs % 10);
+        rhs = rhs / 10;
+    }
+
+    rhs_vec
+        .into_iter()
+        .rev()
+        .fold(lhs, move |lhs, val| lhs * 10 + val)
+}
 fn try_evaluate_(
     lhs: RhsType,
     current_val: RhsType,
@@ -40,10 +53,26 @@ fn try_evaluate_(
 
     let operand = operands.pop_front().unwrap();
     if lhs == current_val + operand || lhs == current_val * operand {
-        Evaluation::Valid
-    } else if try_evaluate_(lhs, current_val * operand, operands.clone(), use_concat)
+        return Evaluation::Valid;
+    }
+
+    if use_concat && lhs == concat_nums(current_val, operand) {
+        return Evaluation::Valid;
+    }
+
+    if try_evaluate_(lhs, current_val * operand, operands.clone(), use_concat)
         == Evaluation::Invalid
     {
+        if use_concat
+            && try_evaluate_(
+                lhs,
+                concat_nums(current_val, operand),
+                operands.clone(),
+                use_concat,
+            ) == Evaluation::Valid
+        {
+            return Evaluation::Valid;
+        }
         try_evaluate_(lhs, current_val + operand, operands.clone(), use_concat)
     } else {
         Evaluation::Valid
@@ -55,6 +84,13 @@ fn try_evaluate(expression: Equation) -> Evaluation {
 
     let first_op = rhs_ops.pop_front().expect("getting first operand");
     return try_evaluate_(lhs, first_op, rhs_ops.clone(), false);
+}
+
+fn try_evaluate_with_concat(expression: Equation) -> Evaluation {
+    let (lhs, mut rhs_ops) = expression;
+
+    let first_op = rhs_ops.pop_front().expect("getting first operand");
+    return try_evaluate_(lhs, first_op, rhs_ops.clone(), true);
 }
 
 pub(crate) fn find_valid_equations(input: &str) -> RhsType {
@@ -71,7 +107,14 @@ pub(crate) fn find_valid_equations(input: &str) -> RhsType {
 
 pub(crate) fn find_valid_equations_concat(input: &str) -> RhsType {
     let equations: Vec<Equation> = input.lines().map(create_equation).collect();
-    0
+
+    equations
+        .into_iter()
+        .filter(|expression| {
+            let result = try_evaluate_with_concat(expression.clone());
+            result == Evaluation::Valid
+        })
+        .fold(0, move |acc, (lhs, _)| acc + lhs)
 }
 
 #[cfg(test)]
@@ -97,5 +140,15 @@ mod tests {
     fn test_evaluating_expression() {
         let equation: Equation = (12, VecDeque::from([3, 4]));
         expect_eq!(Evaluation::Valid, try_evaluate(equation));
+    }
+
+    #[gtest]
+    fn test_concat() {
+        expect_eq!(56, concat_nums(5, 6));
+    }
+
+    #[gtest]
+    fn test_concat_big_numbers() {
+        expect_eq!(2378, concat_nums(23, 78));
     }
 }
