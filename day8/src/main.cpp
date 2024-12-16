01d73eeb2d279fd9bb890292f449281bb8f63db9
Ming <mail@zyming.me>
2024-12-16 14:43:53 +0100

    Complete day 8 problem 1

--- day8/src/main.cpp
+++ day8/src/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <sstream>
+
+#include "solution.h"
 
 int main()
 {
-  std::cout << "Hello world\n";
+  std::string line;
+  std::stringstream inputSs;
+  while (std::getline(std::cin, line, '\n')) {
+    inputSs << line << "\n";
+  }
+
+  const auto input = inputSs.str();
+  const auto resultsProblem1 = solution::solveProblem1(input);
+  std::cout << "Problem input is:\n" << input;
+  std::cout << "problem 1 solution: " << resultsProblem1 << std::endl;
 }
